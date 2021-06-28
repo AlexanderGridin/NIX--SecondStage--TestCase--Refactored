b8bf6ca2d54478b2a10744b7bafeca9e5e715119
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-06-29 00:51:17 +0300

    Небольшие изменения в структуре проекта

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+import {getDataFromUrl} from './utils.js';
+import Table from './Table/Table.js';
+import Pagination from './Pagination/Pagination.js';
+
 document.addEventListener('DOMContentLoaded', main);
 
 function main(){
@@ -5,9 +9,28 @@ function main(){
     menuButtonSelector: '.horizontal-nav__mobile-button', 
     menuListSelector: '.horizontal-nav__list',
   });
+
+  initTableAndPagination();
 }
 
 // Functions
+function initTableAndPagination(){
+  let dataUrl = 'http://f0541354.xsph.ru/tasks';
+  let table = new Table('#tasks-table');
+  let pagination = new Pagination('.pagination');
+
+  pagination.setElementForPagging(table);
+
+  table.enableSortByFields('efficiency', 'myTimeSpentByPeriod', 'totalTimeSpentByAll', 'estimation');
+  table.setFieldsForSummary('totalTimeSpentByAll', 'myTimeSpentByPeriod');
+  table.setLoader();
+
+  getDataFromUrl(dataUrl).then((data) => {
+    table.build(data);
+    table.removeLoader();
+  });
+}
+
 function handleMobileMenuButton({menuButtonSelector, menuListSelector}){
   let menuButton = document.querySelector(menuButtonSelector);
   let menuList = document.querySelector(menuListSelector);
